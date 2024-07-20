4f62b6d7b43cbf44327f1e6d6881d92e65f5bcaa
simonn70 <simonadjei70@gmail.com>
2024-07-20 05:43:59 -0700

    update stuff

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,8 @@ import {  clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 
 const protectedRoute = createRouteMatcher([
-  '/dashboard',
-  '/upcoming',
+  '/',
+  '/profile',
   '/meeting(.*)',
   '/previous',
   '/recordings',
